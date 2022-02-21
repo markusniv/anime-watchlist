a02bccbe44a3fae78ff03536eebf4afc929dc2dc
Markus Nivasalo <markus.nivasalo@outlook.com>
2022-02-21 13:26:15 +0200

    Added basic bottom navigation with two fragments.

--- app/src/main/java/com/marsu/animelist/MainActivity.kt
+++ app/src/main/java/com/marsu/animelist/MainActivity.kt
@@ -2,10 +2,23 @@ package com.marsu.animelist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.marsu.animelist.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val bottomNavigationView = binding.bottomNavigationView
+        val navController = findNavController(R.id.fragmentContainerView)
+
+        bottomNavigationView.setupWithNavController(navController)
+
     }
 }
